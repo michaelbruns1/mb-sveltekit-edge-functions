430f8201321dc4e744c1050c2390345b9b7dcef1
michaelbruns1 <mb@michaelbruns.one>
2023-05-02 17:39:18 -0700

    added mdsvex support and a mdsvex.d.ts file

--- src/routes/edge/+page.svelte
+++ src/routes/edge/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Details from '$lib/Details.svelte';
+	import Post from '../post.svx';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -8,6 +9,9 @@
 <h1 class="text-3xl font-bold text-blue-400 underline">
   Hello world!
 </h1>
+
+<Post />
+
 
 <div class="info">
 	<Details city={data.city} ip={data.ip} />
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,23 @@
+import { mdsvex } from 'mdsvex';
+import preprocess from 'svelte-preprocess';
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	kit: {
 		adapter: adapter({ runtime: 'edge' }),
 
 		inlineStyleThreshold: 5000
-	}
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		}),
+		mdsvex(mdsvexConfig)
+	]
 };
 
 export default config;
