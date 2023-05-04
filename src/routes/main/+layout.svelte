a052e31f4c263b9af67ae203a2a38f6c8c0494b0
michaelbruns1 <mb@michaelbruns.one>
2023-05-03 19:13:26 -0700

    removed top level layout and rerouted to the main route to have independent route layouts and top level navigation

---
+++ src/routes/main/+layout.svelte
@@ -0,0 +1,60 @@
+<script>
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/all.css';
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../../app.postcss';
+	import { fade } from 'svelte/transition';
+	import '$lib/styles.css';
+	import card from '$lib/twitter-card.jpg';
+	import Card from '$lib/Card.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import { page } from '$app/stores';
+	import { AppShell, AppBar, CodeBlock, storeHighlightJs } from '@skeletonlabs/skeleton';
+	import hljs from 'highlight.js';
+	import 'highlight.js/styles/github-dark.css';
+	storeHighlightJs.set(hljs);
+</script>
+
+<svelte:head>
+	<title>SvelteKit on the edge</title>
+	<meta name="description" content="HTML, dynamically rendered in a city near you" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@vercel" />
+	<meta name="twitter:creator" content="@sveltejs" />
+	<meta name="twitter:title" content="SvelteKit on the edge" />
+	<meta name="twitter:description" content="HTML, dynamically rendered in a city near you" />
+	<meta name="twitter:image" content="{$page.url.origin}{card}" />
+	<meta name="twitter:image:alt" content="The Vercel and Svelte logos" />
+</svelte:head>
+
+<!-- <Card></Card> -->
+
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="header">
+		<AppBar>Skeleton</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<nav class="list-nav">
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/edge">Edge</a></li>
+				<li><a href="/node">Node</a></li>
+			</ul>
+		</nav>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarRight" />
+	<!-- <svelte:fragment slot="pageHeader">PageHeader</svelte:fragment> -->
+
+	<!-- Router Slot -->
+	<span class="block">
+		<slot />
+	</span>
+	<!-- ---- / ---- -->
+
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<svelte:fragment slot="footer"><Footer /></svelte:fragment>
+</AppShell>
---
+++ src/routes/main/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { CodeBlock } from '@skeletonlabs/skeleton';
+</script>
+
+<div>
+	<h1>Welcome to MBPREP</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	<section>
+		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
+	</section>
+	<CodeBlock language="html" code={`<div>This is meta</div>`} />
+	<CodeBlock language="css" code={`.skeleton { color: #bada55; }`} />
+	<CodeBlock language="ts" code={`const skeleton: string = 'awesome';`} />
+</div>
